7c1f5dc2c51e8fdde39af34c0b9ae817a545d54c
kimora <guissendiaye24@gmail.com>
2021-12-22 16:41:39 +0000

    j'ai créer un petit projet de crud permettant d'ajouter des tutoriels dans la base de donnée,de les lister,de les rechercher, de les modifier et enfin de les supprimer

---
+++ src/main/java/com/guissendiaye/springbootcrudmguissendiaye/SpringBootCrudMguisseNdiayeApplication.java
@@ -0,0 +1,13 @@
+package com.guissendiaye.springbootcrudmguissendiaye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudMguisseNdiayeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudMguisseNdiayeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guissendiaye/springbootcrudmguissendiaye/SpringBootCrudMguisseNdiayeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guissendiaye.springbootcrudmguissendiaye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudMguisseNdiayeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
